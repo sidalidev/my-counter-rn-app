3eda1c8406ec8834274250592cb8a3b6628abeda
Sid Bentifraouine <sidbentifraouine@gmail.com>
2020-01-14 09:33:40 +0100

    feat(simple-counter): non below zero counter

--- App.js
+++ App.js
@@ -1,19 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, SafeAreaView } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function incrementCounter() {
+    setCount(count + 1);
+  }
+
+  function decrementCounter() {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
+
+  function renderBelowZeroAlert() {
+    if (count < 1) {
+      return <Text style={{ color: "red" }}>Can't go below 0</Text>;
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Counter App</Text>
+        <View style={styles.countContainer}>
+          <Text>{count}</Text>
+          <View style={styles.actionButtons}>
+            <Button title="-" onPress={decrementCounter} />
+            <Button title="+" onPress={incrementCounter} />
+          </View>
+          {renderBelowZeroAlert()}
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   },
+  countContainer: {
+    paddingTop: 100,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20
+  },
+  actionButtons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
